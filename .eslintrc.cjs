97818165a28aa578c0e473bae909e0405d095385
Pajjaphon Whanchid <97209810+ZFlucKZ@users.noreply.github.com>
2023-11-30 20:13:46 +0700

    Initial : Setup project / eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,55 @@
+// eslint-disable-next-line
+const path = require('path');
+
+/** @type {import('eslint').Linter.Config} */
+const config = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: true,
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    '@typescript-eslint/array-type': 'off',
+    '@typescript-eslint/consistent-type-definitions': 'off',
+    '@typescript-eslint/prefer-nullish-coalescing': 'off',
+    '@typescript-eslint/consistent-type-imports': [
+      'warn',
+      {
+        prefer: 'type-imports',
+        fixStyle: 'inline-type-imports',
+      },
+    ],
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-misused-promises': [
+      2,
+      {
+        checksVoidReturn: { attributes: false },
+      },
+    ],
+  },
+  overrides: [
+    {
+      extends: [
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      ],
+      files: ['*.ts', '*.tsx'],
+      parserOptions: {
+        // eslint-disable-next-line
+        project: path.join(__dirname, 'tsconfig.json'),
+      },
+      rules: {
+        '@typescript-eslint/no-floating-promises': 'off',
+        '@typescript-eslint/no-misused-promises': 'off',
+      },
+    },
+  ],
+};
+
+// eslint-disable-next-line
+module.exports = config;
